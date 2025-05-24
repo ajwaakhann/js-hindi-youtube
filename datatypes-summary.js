43b38e6b4ab522e082908f9014424026937d5a28
ajwaakhann <khanajwa6969@gmail.com>
2025-05-24 19:54:42 +0000

    datatypes-summary

---
+++ datatypes-summary.js
@@ -0,0 +1,35 @@
+// Primitive
+
+// 7types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456346784628n
+
+
+
+// Referece (Non primitive)
+
+//Array, Objects, Function
+
+const heros = ["fahad", "hania", "danial"]
+let myObj =  {
+    name: "ajwa",
+    age: "20",
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
