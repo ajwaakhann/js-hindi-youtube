47e63822084fde0e9fff0886c0d4c31eae23f6b6
ajwaakhann <khanajwa6969@gmail.com>
2025-08-04 17:25:01 +0000

    json talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,5 +1,3 @@
-const { useSyncExternalStore } = require("react")
-
 // const tinderUser =new object()
 const tinderUser = {}
 
@@ -48,10 +46,36 @@ const user = [
 ]
 
 user[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const{courseInstructor: instructor} = course 
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name"= "hitesh",
+//     "coursename"= "js in hindi",
+//     "price"= "free" 
+// }
+
+[
+    {},
+    {},
+    {}
+]
